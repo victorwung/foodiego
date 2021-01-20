c8171a3d268cde7bb44c5c82d1632e116d531230
victorwung <fixedstar1350@gmail.com>
2021-01-20 13:57:06 +0800

    outer input box check

--- public/js/index.js
+++ public/js/index.js
@@ -3,8 +3,15 @@ function outerSearchFood() {
   if (localStorage.getItem('food')) {
     window.localStorage.removeItem('food');
   }
-  if (food.length !== 0) {  
+  // if (food.length !== 0) {  
+  //   window.localStorage.setItem('food', food);
+  // }
+  if (food.length !== 0) {
     window.localStorage.setItem('food', food);
+    // redirect
+    window.location.href='/main.html';
+  } else {
+    Swal.fire('Please try again!', 'Type any food name in the search box.');
   }
-  window.location.href='/main.html';
+  // window.location.href='/main.html';
 }
